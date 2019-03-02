d41b003a85c4d30eb102f1c99b8fe7f494f65c3b
Ismail <cucevicismail@gmail.com>
2019-03-02 15:34:36 +0100

    add index.js

--- project/main/index.js
+++ project/main/index.js
@@ -1,28 +1,16 @@
-const cluster = require('cluster');
-
-
-// Is the file being executed  in master mode?
-if(cluster.isMaster){
-    //Cause index.js to be executed *again* but
-    // in child mode
-   cluster.fork();
-}  else{
-
-    //Im a child, im going to act like a server and do nothing else
+ //Im a child, im going to act like a server and do nothing else
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 
-function doWork(duration){
-  const start = Date.now();
-  while(Date.now() - start < duration){
-     
-  }
-}
-
 app.get('/', (req, res) => {
-    doWork(5000)
-   res.send('Hi There!');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi There!');
+     });
 });
 
+app.get('/fast', (req,res) => {
+    res.send('This was fast!');
+})
+
 app.listen(3000);
-}
