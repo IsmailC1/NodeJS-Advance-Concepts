ef7412a165380f2997924e9095bc91a3d803053a
Ismail <cucevicismail@gmail.com>
2019-03-04 15:39:45 +0100

    save

--- project/main/index.js
+++ project/main/index.js
@@ -2,12 +2,30 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const Worker = require('webworker-threads').Worker;
 
 app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('Hi There!');
-     });
+   
+ const worker = new Worker(function() {  
+    this.onmessage = function() {
+       let counter = 0;
+       while(counter  < 1e9){
+           counter++;
+       } 
+
+       postMessage(counter);
+    };
 });
+
+worker.onmessage = function(myCounter) {
+    console.log(myCounter);
+}
+
+worker.postMessage();
+
+});
+
+
 
 app.get('/fast', (req,res) => {
     res.send('This was fast!');
